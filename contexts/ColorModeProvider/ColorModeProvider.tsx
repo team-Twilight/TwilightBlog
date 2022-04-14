d9c94570fbea3429d9fa52f1e57ff94cdc656395
Kadyo Yang <mrriey00@naver.com>
2022-04-14 19:36:29 +0900

    add setter of colormode

--- contexts/ColorModeProvider/ColorModeProvider.tsx
+++ contexts/ColorModeProvider/ColorModeProvider.tsx
@@ -1,17 +1,26 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+
+export type ColorMode = "white" | "black";
 
 export type ColorModeContextType = {
-    colorMode: "white" | "black";
+    colorMode: ColorMode;
+    onSetColorMode: (colorMode: ColorMode) => void;
 };
 
 export const ColorModeContext = createContext<ColorModeContextType>({
     colorMode: "white",
+    onSetColorMode: () => {},
 });
 
 const ColorModeProvider: React.FC = ({ children }) => {
     // TODO 시간이 낮이냐 밤이냐 따라서 다른 초기값을 줘볼까아?
+
+    const [colorMode, setColorMode] = useState<ColorMode>("white");
+    const onSetColorMode = (colorMode: ColorMode) => {
+        setColorMode(colorMode);
+    };
     return (
-        <ColorModeContext.Provider value={{ colorMode: "white" }}>
+        <ColorModeContext.Provider value={{ colorMode, onSetColorMode }}>
             {children}
         </ColorModeContext.Provider>
     );
