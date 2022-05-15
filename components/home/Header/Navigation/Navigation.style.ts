14bf56d74dcb0bc89ea9a8dfe9eec86c376247fc
WoogLim <elegy74@gmail.com>
2022-05-15 11:19:24 +0900

    네비게이션바 추가

---
+++ components/home/Header/Navigation/Navigation.style.ts
@@ -0,0 +1,68 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+const menuTextStyle = css`
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 2rem;
+  text-transform: capitalize;
+  vertical-align: baseline;
+`;
+
+export const MenuList = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 20px;
+`;
+
+export const LogoImage = styled.img`
+  width: 60px;
+  height: 60px;
+  object-fit: contain;
+  overflow: hidden;
+  box-sizing: border-box;
+`;
+
+export const DropDownMenu = styled.div`
+  max-width: 4.5rem;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: .5rem  
+  box-sizing: border-box
+`;
+
+export const NavBar = styled.div`
+  width: 1036px;
+  height: 98px;
+
+  display: flex;
+  flex-direction: row;
+  align-itmes: center;
+  padding: 0px;
+  box-sizing: border-box;
+  margin: 0 auto;
+`;
+
+export const MenuButton = styled.button`
+  all: unset;
+  cursor: pointer;
+
+  a{
+    all: unset;
+    ${menuTextStyle}
+  }
+`;
+
+export const HomeButton = styled.button`
+  all: unset;
+  cursor: pointer;
+
+  margin: 0.8rem;
+`;
